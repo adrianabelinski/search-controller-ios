58bf4d862d7a9c618faf14e883e92119cb0798e0
Adriana Belinski <adrianabmpls@gmail.com>
2022-11-14 17:22:21 -0600

    Initial Commit

---
+++ SearchController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchController
+//
+//  Created by Adriana Belinski on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
